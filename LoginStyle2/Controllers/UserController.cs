edb4777a299d31a87f3135f8d34ab8691a5a1324
JIM <mentha5681@gmail.com>
2017-05-05 19:47:41 +0800

    加入專案檔案。

---
+++ LoginStyle2/Controllers/UserController.cs
@@ -0,0 +1,32 @@
+﻿using LoginStyle2.Models.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace LoginStyle2.Controllers
+{
+    public class UserController : Controller
+    {
+        // GET: User
+        public ActionResult Logon()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Logon(LogonViewModel pageData)
+        {
+            if (pageData.Account == "skill" &&
+            pageData.Password == "tree")
+            {
+                pageData.Message =$"您使用{pageData.Account}登入成功。";
+            }
+            else
+            {
+                pageData.Message =$"您使用{pageData.Account}登入失敗。";
+            }
+            return View(pageData);
+        }
+        }
+}

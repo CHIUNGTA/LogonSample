1da899542b0ceb94009a90db9032c013fa874b48
JIM <mentha5681@gmail.com>
2017-05-13 17:19:16 +0800

    導入登入畫面，並增加基本前端驗證

--- LoginStyle2/Controllers/UserController.cs
+++ LoginStyle2/Controllers/UserController.cs
@@ -17,16 +17,22 @@ namespace LoginStyle2.Controllers
         [HttpPost]
         public ActionResult Logon(LogonViewModel pageData)
         {
-            if (pageData.Account == "skill" &&
-            pageData.Password == "tree")
+            if (pageData.Account != null &&
+            pageData.Password != null)
             {
-                pageData.Message =$"您使用{pageData.Account}登入成功。";
+                pageData.Account =$"您使用{pageData.Account}登入成功。";
+              return  RedirectToAction("Logoned");//導入登入畫面中
             }
             else
             {
-                pageData.Message =$"您使用{pageData.Account}登入失敗。";
+                pageData.Account =$"您使用{pageData.Account}登入失敗。";
             }
             return View(pageData);
         }
+
+        public ActionResult Logoned()
+        {
+            return View();
         }
+    }
 }
--- LoginStyle2/Models/ViewModels/LogonViewModel.cs
+++ LoginStyle2/Models/ViewModels/LogonViewModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -7,8 +8,11 @@ namespace LoginStyle2.Models.ViewModels
 {
     public class LogonViewModel
     {
+        [EmailAddress]
         public string Account { get; set; }
+        [StringLength(20,MinimumLength =4,ErrorMessage ="需輸入4~20字串之密碼!!")]
         public string Password { get; set; }
-        public string Message { get; set; }
+        //[EmailAddress]
+        //public string Email { get; set; }
     }
 }
